9fd03777f6d0c4847f9f0d09ffaa30bd864d16b9
stevecyj <ycsteven.tsao@gmail.com>
2021-09-08 08:29:55 +0800

    chore(array):random

---
+++ array/random_vals.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+
+let r = _.random(10); //We produce a random value between 0 and 10.
+console.log(r);
+
+r = _.random(5, 10); //Here we produce a random value between 5 and 10.
+console.log(r);
