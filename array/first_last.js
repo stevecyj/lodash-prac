7c37be4c360e82ab1f00d136e07550e95cd981d6
stevecyj <ycsteven.tsao@gmail.com>
2021-09-08 08:09:53 +0800

    chore(array):first_last

---
+++ array/first_last.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+let words = ['sky', 'wood', 'forest', 'falcon', 'pear', 'ocean', 'universe'];
+
+let fel = _.first(words);
+let lel = _.last(words);
+
+console.log(`First element: ${fel}`);
+console.log(`Last element: ${lel}`);
---
+++ version.js
@@ -0,0 +1,4 @@
+import _ from 'lodash';
+
+const ver = _.VERSION;
+console.log(ver);
