137fc3aa618dfb84d30e286b2f7e6fbc643d45c1
stevecyj <ycsteven.tsao@gmail.com>
2021-09-08 08:38:40 +0800

    chore(array):shuffling

---
+++ array/shuffle_fun.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+let words = ['sky', 'wood', 'forest', 'falcon', 'pear', 'ocean', 'universe'];
+
+console.log(_.shuffle(words));
+console.log(_.shuffle(words));
+console.log(_.shuffle(words));
+console.log(words);
+
+// 該示例從初始單詞數組創建三個新的隨機重組數組。
