576cddf4115831fb32b9aa65a6c3217ae9317df5
stevecyj <ycsteven.tsao@gmail.com>
2021-09-15 14:45:47 +0800

    chore(array):sum

---
+++ array/pulling.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+
+let nums = [1, 2, 3, 1, 2, 2, 4, 5, 7, 8];
+ 
+_.pull(nums, 1, 2);
+console.log(nums);
---
+++ array/sum_fun.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+
+let vals = [-2, 0, 3, 7, -5, 1, 2];
+
+let sum = _.sum(vals);
+console.log(sum);
