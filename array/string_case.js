b1f2bbc09ce6e8f038e13a354c0f2eae068070fe
stevecyj <ycsteven.tsao@gmail.com>
2021-09-16 08:49:06 +0800

    chore(string):string_case

---
+++ array/string_case.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+let words = ['sky', 'Sun', 'Blue Island'];
+
+console.log(_.map(words, _.camelCase));
+console.log(_.map(words, _.capitalize));
+console.log(_.map(words, _.kebabCase));
+console.log(_.map(words, _.lowerCase));
+console.log(_.map(words, _.upperCase));
